ec5ff1f59c798405d78e5a84f90ef9e38798e48a
unknown <Ahliddin dev>
2024-11-20 23:33:31 +0500

    code added

---
+++ OOP/Students.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP;
+public class Student
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public string UniversityName { get; set; }
+
+    public void DisplayInfo()
+    {
+        string response = $"Name : {Name} , LastName : {LastName} ," +
+            $" Age : {Age} , Gender : {Gender}, UniversityName : {UniversityName}";
+
+        Console.WriteLine(response);
+    }
+ 
+}
