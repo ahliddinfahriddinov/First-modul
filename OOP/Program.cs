ad6c5b5f0bbe95cebaf2d26e4075202d71ab83e1
unknown <Ahliddin dev>
2024-11-20 23:44:05 +0500

    code added

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -4,233 +4,232 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        //List<Student> students = new List<Student>();
+        List<Student> students = new List<Student>();
 
-        //Student student1 = new Student();
-        //student1.Name = "Ahliddin";
-        //student1.LastName = "Fahriddinov";
-        //student1.Age = 29;
-        //student1.UniversityName = "PDP University";
-        //student1.Gender = "Male";
+        Student firstStudent = new Student();
+        firstStudent.Name = "Ahliddin";
+        firstStudent.LastName = "Fahriddinov";
+        firstStudent.Age = 29;
+        firstStudent.UniversityName = "PDP University";
 
-        //Student student2 = new Student();
-        //student2.Name = "Aziza";
-        //student2.LastName = "Farxodova";
-        //student2.Age = 19;
-        //student2.UniversityName = "oxford University";
-        //student2.Gender = "Female";
-
-        //Student student3 = new Student();
-        //student3.Name = "Azizbek";
-        //student3.LastName = "Bahodirov";
-        //student3.Age = 21;
-        //student3.UniversityName = " Harvard University";
-        //student3.Gender = "Male";
-
-        //Student student4 = new Student()
-        //{
-        //    Name = "Lazizbek",
-        //    LastName = "Nosirov",
-        //    Age = 30,
-        //    UniversityName = "Jaxon tillari universiteti",
-        //    Gender = "Male"
-
-        //};
-        //Student student5 = new Student()
-        //{
-        //    Name = "Kamola",
-        //    LastName = "Olimova",
-        //    Age = 18,
-        //    UniversityName = "Pedogogika Universiteti",
-        //    Gender = "Female"
-
-        //};
-
-        //student1.DisplayInfo();
-        //student2.DisplayInfo();
-        //student3.DisplayInfo();
-        //student4.DisplayInfo();
-        //student5.DisplayInfo();
+        Student secondStudent = new Student();
+        secondStudent.Name = "Aziza";
+        secondStudent.LastName = "Farxodova";
+        secondStudent.Age = 19;
+        secondStudent.UniversityName = "oxford University";
 
 
-        //var Animals = new List<Animal>();
-
-        //Animal firstAnimal = new Animal()
-        //{
-        //    Name = "Gepard",
-        //    Category = "Wild",
-        //    Living = "Africa",
-        //    Weigth = 60,
-        //    Height = 100,
-        //    LifeSpan = 25,
-        //    Age = 13,
-        //    Speed = 12,
-        //    Color = "Yellow Black",
-        //    Feed = "Meat"
-        //};
-        //Animal secondAnimal = new Animal()
-        //{
-        //    Name = "Rabbit",
-        //    Category = "Pet",
-        //    Living = "Asia",
-        //    Weigth = 10,
-        //    Height = 40,
-        //    LifeSpan = 12,
-        //    Age = 4,
-        //    Speed = 60,
-        //    Color = "White",
-        //    Feed = "Vegatable"
-        //};
-
-        //Animal thirdAnimal = new Animal()
-        //{
-        //    Name = "Dolphin",
-        //    Category = "SeaAnimal",
-        //    Living = "Ocean",
-        //    Weigth = 100,
-        //    Height = 150,
-        //    LifeSpan = 25,
-        //    Age = 15,
-        //    Speed = 100,
-        //    Color = "Blue",
-        //    Feed = "Fish"
-        //};
-
-        //firstAnimal.DisplayInfo();
-        //firstAnimal.RemainingLife();
-        //firstAnimal.DistanceTime();
-
-        //secondAnimal.DisplayInfo();
-        //secondAnimal.RemainingLife();
-        //secondAnimal.DistanceTime();
-
-        //thirdAnimal.DisplayInfo();
-        //thirdAnimal.RemainingLife();
-        //thirdAnimal.DistanceTime();
+        Student thirdStudent = new Student();
+        thirdStudent.Name = "Azizbek";
+        thirdStudent.LastName = "Bahodirov";
+        thirdStudent.Age = 21;
+        thirdStudent.UniversityName = " Harvard University";
 
 
-        //var Restaurants = new List<Restaurant>();
-
-        //Restaurant firstRestaurant = new Restaurant()
-        //{
-        //    Name = "EVOS",
-        //    Since = 2010,
-        //    Address = "Toshkent",
-        //    Dishes = "Lavash,Burger,Donar , Xot Dog",
-        //    Employees = 500,
-        //    Price = 25000 - 50000,
-        //    Category = "Fast Food",
-        //    SpecialDish = "Lavash",
-        //    Opening = 11,
-        //    Closing = 24,
+        Student fourthStudent = new Student()
+        {
+            Name = "Lazizbek",
+            LastName = "Nosirov",
+            Age = 30,
+            UniversityName = "Jaxon tillari universiteti",
 
 
-        //};
-        //firstRestaurant.OrderSpecialDish();
-        //firstRestaurant.InfoRestaurant();
-
-        //Console.Write("Enter time :");
-        //var time = int.Parse(Console.ReadLine());
-        //firstRestaurant.OpenOrClosed(time);
-
-
-        //Restaurant secondRestaurant = new Restaurant()
-        //{
-        //    Name = "Apex Pizza",
-        //    Since = 2014,
-        //    Address = "Toshkent",
-        //    Dishes = "Meat pizza,Cheeze pizza,Chikken pizza and etc ",
-        //    Employees = 300,
-        //    Price = 50000,
-        //    Category = "Fast Food",
-        //    SpecialDish = "Season four pizza",
-        //    Opening = 10,
-        //    Closing = 22,
+        };
+        Student fifthStudent = new Student()
+        {
+            Name = "Kamola",
+            LastName = "Olimova",
+            Age = 18,
+            UniversityName = "Pedogogika Universiteti",
 
 
-        //};
+        };
 
-        //secondRestaurant.InfoRestaurant();
-        //secondRestaurant.OrderSpecialDish();
-        //Console.Write("Enter time :");
-        //var time = int.Parse(Console.ReadLine());
-        //secondRestaurant.OpenOrClosed(time);
-
-        //Restaurant thirdRestaurant = new Restaurant()
-        //{
-        //    Name = "Axmad Jo'Ja",
-        //    Since = 2016,
-        //    Address = "Toshkent",
-        //    Dishes = "Shashlik,Barbekyu,Lag'mon,Evropa taomlari",
-        //    Employees = 200,
-        //    Price =  60000,
-        //    Category = "Restaurant",
-        //    SpecialDish = "Qovurilgan tovuq",
-        //    Opening = 10,
-        //    Closing = 23,
+        firstStudent.DisplayInfo();
+        secondStudent.DisplayInfo();
+        thirdStudent.DisplayInfo();
+        fourthStudent.DisplayInfo();
+        fifthStudent.DisplayInfo();
 
 
-    //};
-    //    thirdRestaurant.InfoRestaurant();
-    //    thirdRestaurant.OrderSpecialDish();
+        var Animals = new List<Animal>();
 
-    //    Console.Write("Enter time :");
-    //    var time = int.Parse(Console.ReadLine());
-    //    thirdRestaurant.OpenOrClosed(time);
+        Animal firstAnimal = new Animal()
+        {
+            Name = "Gepard",
+            Category = "Wild",
+            Living = "Africa",
+            Weigth = 60,
+            Height = 100,
+            LifeSpan = 25,
+            Age = 13,
+            Speed = 12,
+            Color = "Yellow Black",
+            Feed = "Meat"
+        };
+        Animal secondAnimal = new Animal()
+        {
+            Name = "Rabbit",
+            Category = "Pet",
+            Living = "Asia",
+            Weigth = 10,
+            Height = 40,
+            LifeSpan = 12,
+            Age = 4,
+            Speed = 60,
+            Color = "White",
+            Feed = "Vegatable"
+        };
+
+        Animal thirdAnimal = new Animal()
+        {
+            Name = "Dolphin",
+            Category = "SeaAnimal",
+            Living = "Ocean",
+            Weigth = 100,
+            Height = 150,
+            LifeSpan = 25,
+            Age = 15,
+            Speed = 100,
+            Color = "Blue",
+            Feed = "Fish"
+        };
+
+        firstAnimal.DisplayInfo();
+        firstAnimal.RemainingLife();
 
 
-        //var Books = new List<Book>();
+        secondAnimal.DisplayInfo();
+        secondAnimal.RemainingLife();
 
-        //Book firstBook = new Book()
-        //{
-        //    Name = "Manupulative dictators",
-        //    Author = "Cergey Guriyev & Daniel Treysman",
-        //    Pages = 280,
-        //    Translator = "Otabek Abdullayev",
-        //    Languages = "Uzbek",
-        //    PublishedYear = 2021,
-        //    Publisher = "Tirilish",
-        //    Price = 69000,
-        //    Genre = "Political",
-        //    Units = 0
-        //};
-        //firstBook.InfoBook();
-        //firstBook.OnlineOrderBook();
-        //firstBook.BooKUnits();
 
-        //Book secondBook = new Book()
-        //{
-        //    Name = "Shaytanat",
-        //    Author = "Tohir Malik",
-        //    Pages = 560,
-        //    Translator = "Otabek Abdullayev",
-        //    Languages = "Uzbek",
-        //    PublishedYear = 2023,
-        //    Publisher = "Tirilish",
-        //    Price = 12400,
-        //    Genre = "Roman",
-        //    Units = 100
-        //};
-        //secondBook.InfoBook();
-        //secondBook.OnlineOrderBook();
-        //secondBook.BooKUnits();
+        thirdAnimal.DisplayInfo();
+        thirdAnimal.RemainingLife();
 
-        //Book thirdBook = new Book()
-        //{
-        //    Name = "Soft Power",
-        //    Author = "Jozev Nay ",
-        //    Pages = 250,
-        //    Translator = "Elbek Nosirov",
-        //    Languages = "Uzbek",
-        //    PublishedYear = 2022,
-        //    Publisher = "Tirilish",
-        //    Price = 58000,
-        //    Genre = "Political",
-        //    Units = 45
-        //};
-        //thirdBook.InfoBook();
-        //thirdBook.OnlineOrderBook();
-        //thirdBook.BooKUnits();
+
+
+        var Restaurants = new List<Restaurant>();
+
+        Restaurant firstRestaurant = new Restaurant()
+        {
+            Name = "EVOS",
+            Since = 2010,
+            Address = "Toshkent",
+            Dishes = "Lavash,Burger,Donar , Xot Dog",
+            Employees = 500,
+            Price = 25000 - 50000,
+            Category = "Fast Food",
+            SpecialDish = "Lavash",
+            Opening = 11,
+            Closing = 24,
+
+
+        };
+        firstRestaurant.OrderSpecialDish();
+        firstRestaurant.InfoRestaurant();
+
+        Console.Write("Enter time :");
+        var time = int.Parse(Console.ReadLine());
+        firstRestaurant.OpenOrClosed(time);
+
+
+        Restaurant secondRestaurant = new Restaurant()
+        {
+            Name = "Apex Pizza",
+            Since = 2014,
+            Address = "Toshkent",
+            Dishes = "Meat pizza,Cheeze pizza,Chikken pizza and etc ",
+            Employees = 300,
+            Price = 50000,
+            Category = "Fast Food",
+            SpecialDish = "Season four pizza",
+            Opening = 10,
+            Closing = 22,
+
+
+        };
+
+        secondRestaurant.InfoRestaurant();
+        secondRestaurant.OrderSpecialDish();
+        Console.Write("Enter time :");
+        var time = int.Parse(Console.ReadLine());
+        secondRestaurant.OpenOrClosed(time);
+
+        Restaurant thirdRestaurant = new Restaurant()
+        {
+            Name = "Axmad Jo'Ja",
+            Since = 2016,
+            Address = "Toshkent",
+            Dishes = "Shashlik,Barbekyu,Lag'mon,Evropa taomlari",
+            Employees = 200,
+            Price = 60000,
+            Category = "Restaurant",
+            SpecialDish = "Qovurilgan tovuq",
+            Opening = 10,
+            Closing = 23,
+
+
+        };
+        thirdRestaurant.InfoRestaurant();
+        thirdRestaurant.OrderSpecialDish();
+
+        Console.Write("Enter time :");
+        var time = int.Parse(Console.ReadLine());
+        thirdRestaurant.OpenOrClosed(time);
+
+
+        var Books = new List<Book>();
+
+        Book firstBook = new Book()
+        {
+            Name = "Manupulative dictators",
+            Author = "Cergey Guriyev & Daniel Treysman",
+            Pages = 280,
+            Translator = "Otabek Abdullayev",
+            Languages = "Uzbek",
+            PublishedYear = 2021,
+            Publisher = "Tirilish",
+            Price = 69000,
+            Genre = "Political",
+            Units = 0
+        };
+        firstBook.InfoBook();
+        firstBook.OnlineOrderBook();
+        firstBook.BooKUnits();
+
+        Book secondBook = new Book()
+        {
+            Name = "Shaytanat",
+            Author = "Tohir Malik",
+            Pages = 560,
+            Translator = "Otabek Abdullayev",
+            Languages = "Uzbek",
+            PublishedYear = 2023,
+            Publisher = "Tirilish",
+            Price = 12400,
+            Genre = "Roman",
+            Units = 100
+        };
+        secondBook.InfoBook();
+        secondBook.OnlineOrderBook();
+        secondBook.BooKUnits();
+
+        Book thirdBook = new Book()
+        {
+            Name = "Soft Power",
+            Author = "Jozev Nay ",
+            Pages = 250,
+            Translator = "Elbek Nosirov",
+            Languages = "Uzbek",
+            PublishedYear = 2022,
+            Publisher = "Tirilish",
+            Price = 58000,
+            Genre = "Political",
+            Units = 45
+        };
+        thirdBook.InfoBook();
+        thirdBook.OnlineOrderBook();
+        thirdBook.BooKUnits();
 
 
     }
--- OOP/Students.cs
+++ OOP/Students.cs
@@ -4,15 +4,14 @@ public class Student
     public string Name { get; set; }
     public string LastName { get; set; }
     public int Age { get; set; }
-    public string Gender { get; set; }
     public string UniversityName { get; set; }
 
     public void DisplayInfo()
     {
         string response = $"Name : {Name} , LastName : {LastName} ," +
-            $" Age : {Age} , Gender : {Gender}, UniversityName : {UniversityName}";
+            $" Age : {Age} , UniversityName : {UniversityName}";
 
         Console.WriteLine(response);
     }
- 
+
 }
