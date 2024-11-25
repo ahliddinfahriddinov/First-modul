9d39156a75260dd4251e4a4f7a66483df84677f2
unknown <Ahliddin dev>
2024-11-25 16:07:10 +0500

    project Event added

---
+++ Homeworks/Lesson 9/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson_9.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public List<string> Attendees { get; set; }
+        public List<string> Tags { get; set; }
+
+
+    }
+}
